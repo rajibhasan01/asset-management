5271f886c9299c5af5f9487c101d27312c5bbecd
rajibhasan01 <hasan.rajib1996@gmail.com>
2022-06-08 15:14:32 +0600

    updated

--- config.ts
+++ config.ts
@@ -7,9 +7,9 @@ export const configData = {
     clientId:
       "104443873174-d492ffnchecain2qsvmkrjei3tldobf9.apps.googleusercontent.com",
     clientSecret: "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",
-    callbackURL: "https://hidden-waters-59301.herokuapp.com/login/auth/google/callback",
-    failUrl : "https://hidden-waters-59301.herokuapp.com/login",
-    successUrl: "https://hidden-waters-59301.herokuapp.com"
+    callbackURL: "https://rajibhasan-asset-management.herokuapp.com/login/auth/google/callback",
+    failUrl : "https://rajibhasan-asset-management.herokuapp.com/login",
+    successUrl: "https://rajibhasan-asset-management.herokuapp.com"
   },
   user: "admin"
 };
